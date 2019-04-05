fae6f89374205756f86ba79733077b48c076cdf2
hamdi1910 <47891350+hamdi1910@users.noreply.github.com>
2019-04-05 17:04:24 +0200

    Update back.h

--- back.h
+++ back.h
@@ -1,3 +1,11 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<SDL/SDL.h>
+#include<SDL/SDL_image.h>
+#include<SDL/SDL_ttf.h>
+
 #ifndef DEF_CONSTANTES
 #define DEF_CONSTANTES
 
