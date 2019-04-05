1ea6b020a28db350bbfb2796aaf4507990f6ab43
hamdi1910 <47891350+hamdi1910@users.noreply.github.com>
2019-04-05 15:55:25 +0200

    Update back.h

--- back.h
+++ back.h
@@ -1,29 +1,20 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<SDL/SDL.h>
-#include<SDL/SDL_image.h>
-#include<SDL/SDL_ttf.h>
-
-
 #ifndef DEF_CONSTANTES
 #define DEF_CONSTANTES
 
+
 typedef struct 
 {
-SDL_Surface *imageDeFond;
-SDL_Rect positionFond;
-}fond;
+SDL_Surface *imageDeFond ;
+SDL_Rect positionFond ;
+STATE STATE ;
+}background ;
 
-#define LARGEUR_FENETRE 1000
-#define HAUTEUR_FENETRE 1000
+void init_background (background *fond );
 
-void init_backgroung() ;
-int aff_background(int argc, char *argv[]);
+void afficher_background (SDL_Surface *screen, background *fond);
+
 
 #endif
-
 
 
 
