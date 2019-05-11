a91f916285beab4326d690cd5212ecbdef3527ba
AlaaDhaouadi99 <mohamedalaa.dhaouadi@esprit.tn>
2019-05-11 01:20:19 -0700

    ajout rootozoom

---
+++ rotozoom/main.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include <SDL/SDL_image.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_rotozoom.h>
+//#include <SDL_gfxBlitFunc.h>
+/**
+* @file main.c
+* @brief Testing Program.
+* @author C Team
+* @version 0.1
+* @date Apr 01, 2015
+*
+* Testing program for background scrollilng
+*
+*/
+ 
+#define TEMPS       30 // Le temps qu'il y a entre chaque augmentation de l'angle.
+ 
+int main(int argc, char *argv[])
+{
+    SDL_Surface *ecran = NULL, *image = NULL, *rotation = NULL;
+    SDL_Rect rect;
+    SDL_Event event;
+    double angle = 0,zoom=0;
+ 
+    int continuer = 1;
+    int tempsPrecedent = 0, tempsActuel = 0, zommy=0;
+ 
+    SDL_Init(SDL_INIT_VIDEO);
+ 
+    ecran = SDL_SetVideoMode(500, 500, 32, SDL_HWSURFACE);
+    SDL_WM_SetCaption("Faire des rotations avec SDL_gfx", NULL);
+ 
+    image =IMG_Load("Snake.png");
+ 
+    rect.x =  150;
+    rect.y =  150;
+ 
+    while(continuer)
+    {
+        SDL_PollEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+                break;
+        }
+ 
+        tempsActuel = SDL_GetTicks();
+        if (tempsActuel - tempsPrecedent > TEMPS)
+        {
+            angle += 5; //On augmente l'angle pour que l'image tourne sur elle-même.
+ 		zoom += 0.005 ; 
+            tempsPrecedent = tempsActuel;
+        }
+        else
+        {
+            SDL_Delay(TEMPS - (tempsActuel - tempsPrecedent));
+        }
+ 
+ 
+        SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 255, 255, 255));
+ 
+        rotation = rotozoomSurface(image, angle,zoom, 5); //On transforme la surface image.
+ 
+        SDL_BlitSurface(rotation , NULL, ecran, &rect); //On affiche la rotation de la surface image.
+        SDL_FreeSurface(rotation); //On efface la surface rotation car on va la redéfinir dans la prochaine boucle. Si on ne le fait pas, cela crée une fuite de mémoire. 
+ 
+        SDL_Flip(ecran);
+    }
+ 
+    SDL_FreeSurface(ecran);
+    SDL_FreeSurface(image);
+    SDL_Quit();
+ 
+    return EXIT_SUCCESS;
+}
+
